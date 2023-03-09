facf85ed88835eb1bb0cb12dc7be1c1219b19b09
Shishir Phadke <shishircoolin@gmail.com>
2023-03-09 21:59:39 +0530

    Initial commit.

--- index.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-
-import Main from './Product';
-import './style.css';
-
-ReactDOM.render(<Main/>, document.getElementById('root'));
