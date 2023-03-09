c83df7efbc47b35d53fc8b2c7cc6ac3a55137e8d
Shishir Phadke <shishircoolin@gmail.com>
2023-03-09 22:11:44 +0530

    Add title.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Title goes here.</h1>
+        <h1>Shishir test title goes here.</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
