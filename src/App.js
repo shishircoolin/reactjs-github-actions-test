153d16e74ebbc7a77701d1f824a37653d5b6b1a1
Shishir Phadke <shishircoolin@gmail.com>
2023-03-15 23:18:49 +0530

    Title name change.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Shishir Phadke - Booboo.</h1>
+        <h1>Shishir Phadke!</h1>
       </header>
     </div>
   );
